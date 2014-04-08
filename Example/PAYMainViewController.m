4bfde11fcf2d7c22d040c5641904f3571d6e66f4
Eldorado234 <seyer@redpeppix.de>
2014-04-08 19:10:54 +0200

    Added examples
    * Added address input example
    * Added list selection example

--- Example/PAYMainViewController.m
+++ Example/PAYMainViewController.m
@@ -8,8 +8,14 @@
 
 #import "PAYMainViewController.h"
 #import <PAYFormSingleLineTextField.h>
+#import <PAYFormButton.h>
 
 @interface PAYMainViewController ()
+
+@property (nonatomic, retain) PAYFormSingleLineTextField *streetTextField;
+@property (nonatomic, retain) PAYFormSingleLineTextField *postalCodeTextField;
+@property (nonatomic, retain) PAYFormSingleLineTextField *cityTextField;
+@property (nonatomic, retain) PAYFormSection *countrySection;
 
 @end
 
@@ -17,12 +23,48 @@
 
 
 - (void)loadStructure {
-    [self buildTableWithBlock:^(id<PAYTableBuilder> tableBuilder) {
-        [tableBuilder addSectionWithName:@"Test" labelStyle:PAYFormTableLabelStyleSimple contentBlock:^(id<PAYSectionBuilder> sectionBuilder) {
-            [sectionBuilder addFieldWithName:@"Test" placeholder:@"Hallo" configureBlock:^(PAYFormSingleLineTextField *formField) {
-                formField.isRequired = YES;
-            }];
+    [self buildTableWithBlock:^(id<PAYTableBuilder> tableBuilder){
+        [tableBuilder addSectionWithName:@"Adress" labelStyle:PAYFormTableLabelStyleSimple contentBlock:^(id<PAYSectionBuilder> sectionBuilder) {
+            
+            self.streetTextField = [sectionBuilder addFieldWithName:@"Street" placeholder:@"your street"
+                                                     configureBlock:^(PAYFormSingleLineTextField *formField) {
+                                                         formField.isRequired = YES;
+                                                     }];
+            
+            self.postalCodeTextField = [sectionBuilder addFieldWithName:@"Postal code" placeholder:@"your postal code"
+                                                         configureBlock:^(PAYFormSingleLineTextField *formField) {
+                                                             formField.isRequired = YES;
+                                                             formField.textField.keyboardType = UIKeyboardTypeNumberPad;
+                                                         }];
+            self.cityTextField = [sectionBuilder addFieldWithName:@"City" placeholder:@"your city"
+                                                   configureBlock:^(PAYFormSingleLineTextField *formField) {
+                                                       formField.isRequired = YES;
+                                                   }];
         }];
+        
+        [tableBuilder addSectionWithName:@"Country" labelStyle:PAYFormTableLabelStyleSimple contentBlock:^(id<PAYSectionBuilder> sectionBuilder) {
+            NSArray *countries = @[@[@"United States", @"usa"], @[@"Germany", @"de"], @[@"Spain", @"es"]];
+            for (NSArray *country in countries) {
+                [sectionBuilder addButtonWithText:country[0] style:PAYFormButtonStyleIconSelection
+                                   selectionBlock:^(PAYFormView *formButton) {
+                                       
+                                       
+                                   }
+                                   configureBlock:^(PAYFormButton *formButton) {
+                                       if ([country[0] isEqualToString:@"United States"]) {
+                                           [formButton requestSelection];
+                                       }
+                                       formButton.iconView.image = [UIImage imageNamed:country[1]];
+                                   }];
+            }
+            
+        }];
+        
+        tableBuilder.finishOnLastField = YES;
+        tableBuilder.selectFirstField = YES;
+        tableBuilder.formSuccessBlock = ^{
+            [self performSegueWithIdentifier:@"next" sender:self];
+        };
     }];
 }
 
