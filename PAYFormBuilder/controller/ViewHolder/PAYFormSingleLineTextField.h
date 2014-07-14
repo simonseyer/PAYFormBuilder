350ccec57265980723ef6a5ffd9bf53f1556c334
Eldorado234 <seyer@redpeppix.de>
2014-07-14 16:51:32 +0200

    Commented single line textfield

--- PAYFormBuilder/controller/ViewHolder/PAYFormSingleLineTextField.h
+++ PAYFormBuilder/controller/ViewHolder/PAYFormSingleLineTextField.h
@@ -8,16 +8,47 @@
 
 #import "PAYFormTextField.h"
 
+/**
+ *  A form view with a single line textfield
+ */
 @interface PAYFormSingleLineTextField : PAYFormTextField
 
+/**
+ *  The label that is display in front of the textfield
+ */
 @property (nonatomic, retain) UILabel *label;
+
+/**
+ *  The textfield component
+ */
 @property (nonatomic, retain) UITextField *textField;
+
+/**
+ *  A text field delegate that is attached to the textfield
+ */
 @property (nonatomic, weak) id<UITextFieldDelegate> delegate;
 
+/**
+ *  When true, the textfield is expanded to the whole width
+ *  of the form row when it is focused.
+ *  When false, it stays the sames size on focus.
+ */
 @property (nonatomic, assign) BOOL expanding;
+
+/**
+ *  The default frame of the control
+ */
 @property (nonatomic, assign) CGRect defaultFrame;
+
+/**
+ *  The frame, when the textfield is expanded
+ */
 @property (nonatomic, assign) CGRect expandedFrame;
 
+/**
+ *  Enables the secure input mode. Use this, 
+ *  for allowing the user to enter passwords.
+ */
 - (void)activateSecureInput;
 
 @end
