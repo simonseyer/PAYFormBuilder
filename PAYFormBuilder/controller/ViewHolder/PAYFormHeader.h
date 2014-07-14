2c4d51c23c32fbcc9ab451df173da46145e406ee
Eldorado234 <seyer@redpeppix.de>
2014-07-14 17:23:20 +0200

    Commented header

--- PAYFormBuilder/controller/ViewHolder/PAYFormHeader.h
+++ PAYFormBuilder/controller/ViewHolder/PAYFormHeader.h
@@ -8,10 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-
+/**
+ *  Holds the content of a section header
+ */
 @interface PAYFormHeader : NSObject
 
+/**
+ *  The view, that is visible in the header
+ */
 @property (nonatomic, retain) UIView *view;
+
+/**
+ *  The label, that contains the name of the section
+ */
 @property (nonatomic, retain) UILabel *label;
 
 @end
