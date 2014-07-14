d166a88d49437f525860436a59e9597533029ee4
Eldorado234 <seyer@redpeppix.de>
2014-07-14 18:22:39 +0200

    Organized imports

--- PAYFormBuilder/PAYFormBuilder.h
+++ PAYFormBuilder/PAYFormBuilder.h
@@ -12,21 +12,21 @@
 #import "PAYSectionBuilder.h"
 #import "PAYButtonGroupBuilder.h"
 
-#import "PAYFormSection.h"
-#import "PAYHeaderView.h"
+#import "PAYFormTableViewController.h"
 
-#import "PAYFormTextField.h"
-#import "PAYFormSwitch.h"
-#import "PAYTextLabel.h"
-#import "PAYFormButton.h"
-#import "PAYFormButtonGroup.h"
+#import "PAYFormView.h"
 #import "PAYFormSingleLineTextField.h"
 #import "PAYFormMultiLineTextField.h"
-
-#import "PAYFormTableViewController.h"
+#import "PAYFormButton.h"
+#import "PAYFormButtonGroup.h"
+#import "PAYFormSwitch.h"
+#import "PAYFormHeader.h"
 
 #import "PAYFormView+PAYFormDefaultErrorHandler.h"
 #import "PAYErrorCodes.h"
 #import "NSError+PAYComfort.h"
+
+#import "PAYHeaderView.h"
+#import "PAYTextLabel.h"
 
 #import "UIColor+PAYHex.h"
