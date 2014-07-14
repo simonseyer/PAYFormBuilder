b5a679879852a5c4601190cc4dfb31ef5843cc53
Eldorado234 <seyer@redpeppix.de>
2014-07-14 17:22:07 +0200

    Commented switch

--- PAYFormBuilder/controller/ViewHolder/PAYFormSwitch.h
+++ PAYFormBuilder/controller/ViewHolder/PAYFormSwitch.h
@@ -9,13 +9,29 @@
 #import "PAYFormView.h"
 #import "PAYValidatableFormCell.h"
 
+/**
+ *  A form view with a switch control
+ */
 @interface PAYFormSwitch : PAYFormView <PAYValidatableFormCell>
 
+/**
+ *  The label, that is displayed in front of the switch
+ */
 @property (nonatomic, retain) UILabel *label;
+
+/**
+ *  The switch control
+ */
 @property (nonatomic, retain) UISwitch *switchControl;
 
+/**
+ *  Validates, that the control is selected
+ */
 @property (nonatomic, assign) BOOL isRequired;
 
+/**
+ *  True, when the switch is turned on, false otherwise
+ */
 @property (nonatomic, assign) BOOL value;
 
 @end
