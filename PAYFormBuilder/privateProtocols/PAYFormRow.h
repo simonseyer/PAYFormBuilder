f6265c265fa7dc9ec56831e073e333e5f21ad9db
Eldorado234 <seyer@redpeppix.de>
2014-11-04 16:01:15 +0100

    Commented PAYFormRow

--- PAYFormBuilder/privateProtocols/PAYFormRow.h
+++ PAYFormBuilder/privateProtocols/PAYFormRow.h
@@ -8,10 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  High level protocol to describe one form row.
+ */
 @protocol PAYFormRow <NSObject>
 
+/**
+ *  Get the view of the form row. Each row is rendered
+ *  as a UITableViewCell, so you get this cell to manipulate it.
+ *
+ *  @return <#return value description#>
+ */
 - (UITableViewCell *)view;
 
+/**
+ *  Handles the selection of a row. Do NOT call this method by
+ *  yourself.
+ */
 - (void)didSelectRow;
 
 @end
